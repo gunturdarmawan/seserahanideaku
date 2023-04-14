14612a59569e6ca362f6379cd60e4a6ba0d7a343
guntur darmawan <19523166@students.uii.ac.id>
2023-04-15 03:21:03 +0700

    versi pertama

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily:{
+         customfont :  "'Sacramento', cursive",   
+      },  
+      colors: {
+        primary: '#ff1493',
+        secondary: '#64748b',
+        dark: '#0f172a',
+        pinky: '#ff1493',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
